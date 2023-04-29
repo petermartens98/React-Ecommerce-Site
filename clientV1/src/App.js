f5ec9b98e001b2a399dcca0aaf7f9b1186202c44
Peter Martens <87671757+petermartens98@users.noreply.github.com>
2023-04-29 19:02:48 -0400

    Add files via upload

---
+++ clientV1/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+          <footer>
+            <p>Created by Peter Martens</p>
+            <a href="https://github.com/petermartens98/React-Ecommerce-Site">
+            <img className="github-logo" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub logo" />
+            </a>
+          </footer>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ clientV1/src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <Link to="/">Peter's Tech Shop</Link>
+      </div>
+      <div className="links">
+        <Link to="/"> Shop </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
+    </div>
+  );
+};
